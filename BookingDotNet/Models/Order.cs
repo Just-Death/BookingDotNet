8141a6094e1ddd8e733c412660afa7fa7dbe6189
Amir <amirzhan.tolebay@gmail.com>
2020-04-24 23:56:49 +0600

    Добавьте файлы проекта.

---
+++ BookingDotNet/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingDotNet.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string UserIds { get; set; }
+        public string KitchenIds { get; set; }
+    }
+}
---
+++ BookingDotNet/Models/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingDotNet.Models
+{
+    public class Room
+    {
+        public int RoomId { get; set; }
+        public int RoomQuantity { get; set; }
+        public Category category { get; set; }
+        public int BookedUserId { get; set; }
+    }
+}
